488c308e023f6b0a3bde7d44dd060159fb101264
GhostyCeh <41862824+GhostyCeh@users.noreply.github.com>
2019-11-20 10:40:15 -0600

    Update LPProxies.py

--- LPProxies.py
+++ LPProxies.py
@@ -2,6 +2,8 @@
 import asyncio, os, colorama
 from proxybroker import Broker
 from colorama import *
+import fileinput
+import sys
 
 
 async def save(proxies, filename):
@@ -20,13 +22,15 @@ def main():
     proxies = asyncio.Queue()
     broker = Broker(proxies)
     tasks = asyncio.gather(broker.find(types=['HTTP', 'HTTPS'], limit= 99999),
-                           save(proxies, filename='proxy.txt'))
+                           save(proxies, filename='proxies.txt'))
     loop = asyncio.get_event_loop()
     loop.run_until_complete(tasks)
+    for line in fileinput.input(['./proxies.txt'], inplace=True):
+        sys.stdout.write('HTTP|{l}'.format(l=line))    
 
 
 if __name__ == '__main__':
     print("  - HTTP Proxy Gathering -  ")
     print("     CTRL + C To STOP       ")
-    print("     File = proxy.txt       ")
+    print("     File = proxies.txt       ")
     main()

